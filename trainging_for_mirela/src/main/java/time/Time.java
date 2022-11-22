ce688f17bfb8fd9db8776a289eb5e68270dc1caf
dinom <dinomc222@mail.ru>
2022-11-22 21:07:35 +0200

    Time changed

--- trainging_for_mirela/src/main/java/time/Time.java
+++ trainging_for_mirela/src/main/java/time/Time.java
@@ -1,13 +1,24 @@
 package time;
 
-import java.lang.*;
 import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Time implements Comparable {
-    int minute, second, hour;
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    private int minute, second, hour;
 
     public Time() {
         this(0, 0, 0);
@@ -27,6 +38,31 @@ public class Time implements Comparable {
         setSecond(second);
     }
 
+    public static Time random() {
+        Time time = new Time();
+        SecureRandom random = new SecureRandom();
+        random.nextInt(10);
+        time.setHour(random.nextInt(24));
+        time.setMinute(random.nextInt(60));
+        time.setSecond(random.nextInt(60));
+        return time;
+    }
+
+    public static void main(String args[]) {
+
+        List<Time> objects = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            objects.add(random());
+            System.out.println(objects.get(i));
+        }
+        System.out.println("\n\n\nSorted time: \n\n\n");
+
+        Collections.sort(objects);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(objects.get(i));
+        }
+
+    }
 
     public void setHour(int hour) {
         if (hour >= 0 && hour < 24) {
@@ -52,58 +88,16 @@ public class Time implements Comparable {
         return String.format("%d:%d:%d", hour, minute, second);
     }
 
-
-    public static Time random() {
-        Time time = new Time();
-        SecureRandom random = new SecureRandom();
-        random.nextInt(10);
-        time.setHour(random.nextInt(24));
-        time.setMinute(random.nextInt(60));
-        time.setSecond(random.nextInt(60));
-        return time;
-    }
-
-
-    public static void main(String args[]) {
-
-        List<Time> objects = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            objects.add(random());
-            System.out.println(objects.get(i));
-        }
-        System.out.println("\n\n\nSorted time: \n\n\n");
-
-        objects = sort(objects);
-        for (int i = 0; i < 10; i++) {
-            System.out.println(objects.get(i));
-        }
-
-    }
-
-    public static List<Time> sort(List<Time> list){
-        for(int i = 0; i<list.size()-1; i++)
-        {
-            for (int j = i+1; j<list.size(); j++)
-            {
-                if(list.get(i).compareTo(list.get(j))>0)
-                {
-                    Time temp = list.get(i);
-                    list.set(i, list.get(j));
-                    list.set(j, temp);
-                }
-            }
-        }
-        return list;
-    }
-
-
     @Override
     public int compareTo(Object time2) {
         Time time = (Time) time2;
-        if (this.hour == time.hour && this.minute == time.minute && this.second == time.second) {
-            return 0;
-        } else if (this.hour > time.hour && this.minute > time.minute && this.second > time.second)
-            return -1;
-        else return 1;
+        int cmp = (this.hour - time.hour);
+        if (cmp == 0) {
+            cmp = (this.minute - time.minute);
+            if (cmp == 0) {
+                cmp = (this.second - time.second);
+            }
+        }
+        return cmp;
     }
 }
