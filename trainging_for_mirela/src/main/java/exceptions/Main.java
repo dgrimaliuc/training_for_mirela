9af8eb04c67d0c50a5fb7a072a1ace4c23e61681
dinom <dinomc222@mail.ru>
2022-12-27 22:32:48 +0200

    Task text file reader created

--- trainging_for_mirela/src/main/java/exceptions/Main.java
+++ trainging_for_mirela/src/main/java/exceptions/Main.java
@@ -1,16 +1,29 @@
 package exceptions;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
     public static void main(String[] args) {
-        try (Resource res = new Resource()) {
-            throw new MyPersonalException();
+        String text = readFileAsString(
+                "src/main/resources/text.txt");
 
+        System.out.println(text);
+    }
 
-        } catch (Error | MyPersonalException e) {
-            System.out.println(e.getMessage());
+    public static String readFileAsString(String fileName) {
+        String data;
+        try {
+            data = new String(Files.readAllBytes(Paths.get(fileName)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
-
+        return data;
     }
 }
